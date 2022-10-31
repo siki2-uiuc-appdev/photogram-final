d15874c564ff0152e219ba152ff793d2b5938226
siki2-uiuc <siki2@illinois.edu>
2022-10-31 16:18:18 +0000

    Began navbar creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def index
     matching_users = User.all
 
-    @list_of_users = matching_users.order({ :username => :desc })
+    @list_of_users = matching_users.order({ :username => :asc })
     render({ :template => "users/index.html.erb"})
   end
 
