8d3eb6c5d37f5c3b57944d50341f5d00fdae0631
siki2-uiuc <siki2@illinois.edu>
2022-11-01 20:40:25 +0000

    Added like photo functionality to photos show page

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -24,7 +24,7 @@ class LikesController < ApplicationController
 
     if the_like.valid?
       the_like.save
-      redirect_to("/likes", { :notice => "Like created successfully." })
+      redirect_to("/photos/#{the_like.photo_id}", { :notice => "Like created successfully." })
     else
       redirect_to("/likes", { :alert => the_like.errors.full_messages.to_sentence })
     end
@@ -51,6 +51,6 @@ class LikesController < ApplicationController
 
     the_like.destroy
 
-    redirect_to("/likes", { :notice => "Like deleted successfully."} )
+    redirect_to("/photos/#{the_like.photo_id}", { :notice => "Like deleted successfully."} )
   end
 end
